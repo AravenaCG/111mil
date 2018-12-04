baa8aee5c6f2dcb34299d879f30ccff41497fefa
AravenaCG <cristian.g.aravena@gmail.com>
2018-12-04 00:25:33 -0300

    No sale interface Iterator

---
+++ POO/U5/Arraylist011/src/arraylist011/Arraylist011.java
@@ -0,0 +1,26 @@
+/*
+ * Enelsiguienteejemplosemuestraun ArrayList denúmerosenteros.
+ */
+package arraylist011;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author jlfum
+ */
+public class Arraylist011 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+ArrayList<Integer> a = new ArrayList<Integer>();
+a.add(18); a.add(22); a.add(-30);
+System.out.println("Nº de elementos: " + a.size());
+System.out.println("El elemento que hay en la posición 1 es " + a.get(1));
+}
+
+        // TODO code application logic here
+    }
+    
+
